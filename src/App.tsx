c450474d588c4dfd53faa0bab703c14af1493ba9
ogunbor <ogunborjerry@gmail.com>
2023-05-23 13:55:35 +0100

    dashboard settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
+import { Dashboard } from './pages/dashboard/dashboard';
 import { customTheme } from './theme/customTheme';
 
 const App: FC = (): ReactElement => {
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
-      <h1>Hello World</h1>
+      <Dashboard />
     </ThemeProvider>
   );
 };
